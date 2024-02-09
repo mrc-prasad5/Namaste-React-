05cb4ca24fb865f52280af3adb4d24aa03a988ad
M R C Prasad <mrc.prasad5@gmail.com>
2024-02-09 10:38:36 +0530

    Episode - 3 commits

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import React from "react";
 import ReactDOM   from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},[
-                 React.createElement("div",{id:"child1"},[
-                  React.createElement("h1",{},"I am a h1 tag"),
-                  React.createElement("h2",{},"I am a h2 tag"),
-                  React.createElement("h3",{},"I am a h3 tag")
-                 ]),
-                  React.createElement("div",{id:"child2"},[
-                    React.createElement("h1",{},"I am a h1 tag"),
-                    React.createElement("h2",{},"I am a h2 tag"),
-                    React.createElement("h3",{},"I am a h3 tag")
-                  ]),
-                  React.createElement("div",{id:"child3"},[
-                    React.createElement("h1",{},"I am a h1 tag"),
-                    React.createElement("h2",{},"I am a h2 tag"),
-                    React.createElement("h3",{},"I am a h3 tag")
-                  ])
-              ]);
+let num = 1000;
+
+//React core syntax for creating react element
+const heading = React.createElement("h1",{id:"heading"},"Welcome to React from core react");
+
+//React Element using JSX
+const sample = <div>Sample react element to show inside another react element</div>
+
+
+//Functional component to create react element using normal functions.
+const Title =  function ()  {
+                    return <div id="heading" className="root" tabIndex="1">
+                                Welcome to React Functional Child Component.
+                            </div>;  
+                }
+
+//Including an element and a component inside a js variable                
+const element = <div>
+                  {sample} 
+                  {Title()} 
+                  Including react element inside a functional component
+                </div>;
+
+
+
+// Functional component using arrow function.
+const HeadingComponent = () => {
+                            return <div id="heading" className="root" tabIndex="1">
+                                        Welcome to React Functional Parent Component.
+                                        {console.log('console works inside functional component')}
+                                        {element}
+                                        <Title/>
+                                        <h2>{num}</h2>
+                                    </div>; 
+                        }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<HeadingComponent/>);

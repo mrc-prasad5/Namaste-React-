3b15c1a120cff6dc564d209472cd81dac4d66a57
M R C Prasad <mrc.prasad5@gmail.com>
2025-03-26 19:41:01 +0530

    Excercise-1 Changes

--- App.js
+++ App.js
@@ -1,41 +1,22 @@
-import React from "react";
-import ReactDOM   from "react-dom/client";
+//Traditonal React Approach
 
-let num = 1000;
-
-//React core syntax for creating react element
-const heading = React.createElement("h1",{id:"heading"},"Welcome to React from core react");
-
-//React Element using JSX
-const sample = <div>Sample react element to show inside another react element</div>
-
-
-//Functional component to create react element using normal functions.
-const Title =  function ()  {
-                    return <div id="heading" className="root" tabIndex="1">
-                                Welcome to React Functional Child Component.
-                            </div>;  
-                }
-
-//Including an element and a component inside a js variable                
-const element = <div>
-                  {sample} 
-                  {Title()} 
-                  Including react element inside a functional component
-                </div>;
-
-
-
-// Functional component using arrow function.
-const HeadingComponent = () => {
-                            return <div id="heading" className="root" tabIndex="1">
-                                        Welcome to React Functional Parent Component.
-                                        {console.log('console works inside functional component')}
-                                        {element}
-                                        <Title/>
-                                        <h2>{num}</h2>
-                                    </div>; 
-                        }
+const parent = React.createElement(
+  'div',
+  {id:'parent'},
+  React.createElement(
+    'div',
+    {id:'child'},
+    [
+      React.createElement('h1',{id:'h1'},'I am h1 tag!...'),
+      React.createElement('h2',{id:'h2'},'I am h2 tag!....'),
+      React.createElement('h3',{id:'h3'},'I am h3 tag!...'),
+      React.createElement('h4',{id:'h4'},'I am h4 tag!...'),
+      React.createElement('h5',{id:'h5'},'I am h5 tag!...'),
+      React.createElement('h6',{id:'h6'},'I am h6 tag!...')
+    ])
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HeadingComponent/>);
+setTimeout(() => {
+  root.render(parent);  
+}, 10000);
